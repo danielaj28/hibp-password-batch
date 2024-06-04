71864a68e16ab6d1260b7a2a22d4c917067d941d
Daniel Ward <danielaj_28@hotmail.co.uk>
2024-06-04 11:53:53 +0100

    Progress, faster, clearer

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var crypto = require("crypto");
 var axios = require("axios");
 var fs = require("fs");
 
-let rateLimit = 3;
+let rateLimit = 0;
+let [checked, total, safe, breach] = [0, 0, 0, 0];
+let breached = [];
 
 function gethash(plaintext) {
   return crypto.createHash("sha1").update(plaintext).digest("hex");
@@ -10,15 +12,23 @@ function gethash(plaintext) {
 
 function check() {
   if (items.length == 0) {
-    console.log("DONE")
+    if (breached == 0) {
+      console.log("👏  No breached passwords  👏");
+    } else {
+      console.log("☠️  Breached passwords below  ☠️");
+      breached.forEach((breachedPassword) => {
+        console.log(`☠️  ${breachedPassword}`);
+      });
+    }
     return;
   }
+
+  stats();
+
   password = items.pop();
   let hash = gethash(password);
-  //console.log(hash);
   let hashStart = hash.substring(0, 5);
   let hashEnd = hash.substring(5, hash.length);
-  //console.log(hash, hashStart, hashEnd);
 
   //Get by range
   axios({
@@ -29,16 +39,17 @@ function check() {
       switch (response.status) {
         case 200:
           //Check list
-          //console.debug(response.data.split("\r\n"));
           let result = response.data
             .split("\r\n")
             .filter((a) => a.toLowerCase().startsWith(hashEnd.toLowerCase()));
 
           if (result.length == 0) {
-            console.log(`😁   Safe: ${password}`);
+            safe++;
           } else {
-            console.log(`💀 Breach: ${password}`);
+            breached.push(password);
+            breach++;
           }
+          checked++;
           setTimeout(check, rateLimit * 1000);
 
           //Return result
@@ -59,4 +70,19 @@ function check() {
 }
 
 let items = fs.readFileSync("./items.txt").toString().split("\r\n");
+total = items.length;
 check();
+
+function stats() {
+  let donePercent = 100;
+  let completedPercent = Math.ceil((checked / total) * 100);
+  let progressBar = "";
+  for (let i = 0; i < completedPercent; i++) {
+    progressBar += "#";
+  }
+  for (let i = 0; i < 100 - completedPercent; i++) {
+    progressBar += "-";
+  }
+  console.clear();
+  console.log(`[${progressBar}] ${checked}/${total} ☠️  ${breach}`);
+}
